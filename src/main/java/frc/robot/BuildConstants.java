8167d19a83004bd289f85b06593cd586474623dd
Emmanuel <ejeffers22@gmail.com>
2025-04-25 14:14:01 -0400

    added template code from mechanical advantage

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,17 @@
+package frc.robot;
+
+/** Automatically generated file containing build version information. */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "OffseasonBot";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 1;
+  public static final String GIT_SHA = "23c9049a1a1bc54443f012bf18a6180f0f4f011d";
+  public static final String GIT_DATE = "2025-04-25 13:10:39 EDT";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2025-04-25 14:05:26 EDT";
+  public static final long BUILD_UNIX_TIME = 1745604326545L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants() {}
+}
